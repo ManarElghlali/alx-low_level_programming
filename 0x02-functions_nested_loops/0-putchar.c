4bca254518b8cd869bd544e07f34b43058b7b05c
manarElghlali <manarelghlali@gmail.com>
2023-08-15 11:51:02 +0400

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <main.h>
+/**
+ * main - entry point
+ * _putchar - writes the character c to stdout
+ *  Return: On success 0.
+ *
+ */
+int main(void)
+	{
+		_putchar('_');
+		_putchar('p');
+		_putchar('u');
+		_putchar('t');
+		_putchar('c');
+		_putchar('h');
+		_putchar('a');
+		_putchar('r');
+	return (0);
+	}
+
